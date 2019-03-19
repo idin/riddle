bf5a77cb63cdf248537cc2041f58144e13b2168b
Idin <d@idin.net>
2019-03-18 22:13:52 -0700

    add Box

--- riddle/Box.py
+++ riddle/Box.py
@@ -1,77 +1,71 @@
-import route
+from route import Path
 import atexit
-import send2trash
-import os
 from chronology import get_elapsed_seconds, get_now
+
 
 class Box:
 	def __init__(self, path, save_interval_seconds=60):
-		self._path = route.Path(string=path)
+		self._path = Path(string=path)
 		self._path.make_parent_directory(ignore_if_exists=True)
-
 		self._num_saved_items = None
-
 		self._dict = {}
-		self.load(append=False)
-
 		self._save_interval_seconds = save_interval_seconds
 		self._save_time = get_now()
-
+		self.load(append=False)
 		atexit.register(self.save)
 
-	# working with the file
-	def file_exists(self):
-		if self.path.exists():
-			if self.path.is_file():
-				return True
-			else:
-				raise ValueError(f'Box: {self.path} is not a file!')
-		else:
-			return False
+	def __getstate__(self):
+		return {
+			'path': self._path,
+			'num_saved_items': self._num_saved_items,
+			'dict': self._dict,
+			'save_interval_seconds': self._save_interval_seconds,
+			'save_time': self._save_time
+		}
 
-	def load(self, append=True):
-		if self.file_exists():
-			dictionary = self._path.unpickle(method='pickle')
+	def __setstate__(self, state):
+		self._path = state['path']
+		self._path.make_parent_directory(ignore_if_exists=True)
+		self._num_saved_items = state['num_saved_items']
+		self._dict = state['dict']
+		self._save_interval_seconds = state['save_interval_seconds']
+		self._save_time = state['save_time']
+		self.load(append=True)
+		self.check()
+		atexit.register(self.save)
+
+
+	def check(self, echo=0):
+		echo = max(0, echo)
+		# if the number of items increased by more than 10% o
+		if get_elapsed_seconds(start=self._save_time) > self._save_interval_seconds:
+			self.save(echo=echo)
+
+	@property
+	def path(self):
+		"""
+		:rtype: Path
+		"""
+		self.check()
+		return self._path
+
+	def load(self, append=True, echo=0):
+		echo = max(0, echo)
+		if self.path.exists():
+			dictionary = self.path.unpickle(method='pickle', echo=echo)
 			if append:
 				dictionary.update(self._dict)
 			self._dict = dictionary
 
-	def delete_file(self):
-		if self.file_exists():
-
-			Pickler.delete(path=self._path)
-
-	def save(self, append=True, echo=1):
+	def save(self, echo=0):
 		echo = max(0, echo)
-		if len(self._dict)>0:
-			if echo: print(f'Saving to "{self._path}"')
-			if append:
-				self.load(append=True)
-			Pickler.save(obj=self._dict, path=self._path)
-		else:
-			if echo: print('Nothing to save!')
-			if not append:
-				self.delete_file()
-
+		self.path.pickle(obj=self._dict, echo=echo)
 		self._save_time = get_now()
 
 	@property
-	def file_size(self):
+	def size_bytes(self):
 		self.check()
-		try:
-			return os.path.getsize(self.path)
-		except:
-			return 0
-
-
-	def copy(self):
-		self.check()
-		return self
-
-	@property
-	def path(self):
-		self.check()
-		return self._path
+		return self.path.size_bytes
 
 	@property
 	def names(self):
@@ -88,14 +82,13 @@ class Box:
 		self.check()
 		return self._dict.values()
 
-
-
 	def get_all_names(self):
 		self.check()
 		return self._dict.keys()
 
 	@property
 	def size(self):
+		self.check()
 		return len(self._dict)
 
 	def contains(self, name):
@@ -104,17 +97,19 @@ class Box:
 
 	def get(self, name):
 		self.check()
+		if name not in self._dict:
+			self.load(append=True)
 		return self._dict[name]
 
-	def check(self, echo=0):
-		echo = max(0, echo)
-		# if the number of items increased by more than 10% o
-		if get_elapsed_seconds(start=self._save_time)>self._save_interval_seconds:
-			self.save(echo=echo, append=True)
-
-	def put(self, name, obj):
+	def set(self, name, obj):
 		self._dict[name] = obj
 		self.check()
+
+	def __getitem__(self, item):
+		return self.get(name=item)
+
+	def __setitem__(self, key, value):
+		self.set(name=key, obj=value)
 
 	def remove(self, name):
 		del self._dict[name]
@@ -122,3 +117,4 @@ class Box:
 
 	def flush(self):
 		self._dict = {}
+		self.check()
