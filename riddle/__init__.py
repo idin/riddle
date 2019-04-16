603abd488bc446032d8e1787faac6f0b686f4483
Idin <d@idin.net>
2019-04-15 23:37:46 -0700

    add Folder

--- riddle/__init__.py
+++ riddle/__init__.py
@@ -1,6 +1,7 @@
-
 from .encrypt import encrypt, decrypt
 from .Riddle import Riddle
 from .Box import Box
 from .Locket import Locket
 from .hash import hash
+from .Folder import Folder
+from .make_cached import make_cached
